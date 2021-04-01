e839c593260d6cf44b493f6308ba9d0823324fb7
705336971@qq.com <linfeng@.qq.com>
2021-04-01 21:51:26 +0800

    打印员工信息

---
+++ .history/src/components/ImageUpload/index_20210401101815.vue
@@ -0,0 +1,134 @@
+<template>
+  <div>
+    <el-upload
+      list-type="picture-card"
+      :limit="1"
+      action="#"
+      :on-preview="preview"
+      :file-list="fileList"
+      :class="{disabled: fileComputed }"
+      :on-remove="handleRemove"
+      :on-change="changeFile"
+      :before-upload="beforeUpload"
+      :http-request="upload"
+    >
+      <i class="el-icon-plus" />
+    </el-upload>
+    <el-progress />
+    <el-dialog title="图片" :visible.sync="showDialog">
+      <img :src="imgUrl" style="width:100%" alt="">
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import COS from 'cos-js-sdk-v5' // 引入腾讯云COS包
+// 实例化 COS 对象
+const cos = new COS({
+  SecretId: 'AKIDHCtKZZO7VeX6PYOLyLDqE9sMkskdcdNq ', // 身份识别ID
+  SecretKey: 'OO9fy1jCO3SWVZO9etzxU6mBoDiRuvgD' // 身份秘钥
+})
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+      fileList: [{ url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201710%2F14%2F20171014234316_GcBFK.jpeg&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1619785435&t=4458c7e48128b00c125423d5f764d947' }],
+      showDialog: false,
+      imgUrl: null
+    }
+  },
+  computed: {
+    // 设定一个计算属性 判断是否已经上传完了一张
+    fileComputed() {
+      return this.fileList.length === 1
+    }
+  },
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {
+    preview(file) {
+      console.log(file)
+      this.imgUrl = file.url
+      this.showDialog = true
+    },
+    // 经过打印2个参数  看出 file 是要删除的文件  fileList是删过之后的文件
+    handleRemove(file, fileList) {
+      // console.log(file)
+      // console.log(fileList)
+      console.log(this.fileList)
+      this.fileList = this.fileList.filter(item => item.uid !== file.uid) // 将当前的删除文件排除在外
+    },
+    // 不能用push 因为这个钩子会执行多次
+    changeFile(file, fileList) {
+      this.fileList = fileList.map(item => item)
+      // console.log('1')
+    },
+    beforeUpload(file) {
+      //   先检查文件类型
+      const types = ['image/jpeg', 'image/gif', 'image/bmp', 'image/png']
+      if (!types.some(item => item === file.type)) {
+        //   如果不存在
+        this.$message.error('上传图片只能是 JPG、GIF、BMP、PNG 格式!')
+        return false // 上传终止
+      }
+      // 检查文件大小  5M 1M = 1024KB 1KB = 1024B
+      const maxSize = 5 * 1024 * 1024
+      if (file.size > maxSize) {
+        //   超过了限制的文件大小
+        this.$message.error('上传的图片大小不能大于5M')
+        return false
+      }
+      //   已经确定当前上传的就是当前的这个file了
+      this.currentFileUid = file.uid
+      return true // 最后一定要return  true
+    },
+    // 自定义上传动作 有个参数 有个file对象，是我们需要上传到腾讯云服务器的内容
+    // 进行上传操作
+    // 进行上传操作
+    upload(params) {
+    //   console.log(params.file)
+      if (params.file) {
+        // 执行上传操作
+        cos.putObject({
+          Bucket: 'shuiruohanyu-106-1302806742', // 存储桶
+          Region: 'ap-beijing', // 地域
+          Key: params.file.name, // 文件名
+          Body: params.file, // 要上传的文件对象
+          StorageClass: 'STANDARD' // 上传的模式类型 直接默认 标准模式即可
+          // 上传到腾讯云 =》 哪个存储桶 哪个地域的存储桶 文件  格式  名称 回调
+        }, (err, data) => {
+          // data返回数据之后 应该如何处理
+          console.log(err || data)
+          // data中有一个statusCode === 200 的时候说明上传成功
+          if (!err && data.statusCode === 200) {
+            //   此时说明文件上传成功  要获取成功的返回地址
+            // fileList才能显示到上传组件上 此时我们要将fileList中的数据的url地址变成 现在上传成功的地址
+            // 目前虽然是一张图片 但是请注意 我们的fileList是一个数组
+            // 需要知道当前上传成功的是哪一张图片
+            this.fileList = this.fileList.map(item => {
+              // 去找谁的uid等于刚刚记录下来的id
+              if (item.uid === this.currentFileUid) {
+                // 将成功的地址赋值给原来的url属性
+                return { url: 'http://' + data.Location, upload: true }
+                // upload 为true 表示这张图片已经上传完毕 这个属性要为我们后期应用的时候做标记
+                // 保存  => 图片有大有小 => 上传速度有快又慢 =>要根据有没有upload这个标记来决定是否去保存
+              }
+              return item
+            })
+            // 将上传成功的地址 回写到了fileList中 fileList变化  =》 upload组件 就会根据fileList的变化而去渲染视图
+          }
+        })
+      }
+    }
+
+  }
+}
+</script>
+
+<style>
+.disabled .el-upload--picture-card {
+  display: none
+}
+</style>
---
+++ .history/src/components/ImageUpload/index_20210401114228.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+    <el-upload
+      list-type="picture-card"
+      :limit="1"
+      action="#"
+      :on-preview="preview"
+      :on-remove="handleRemove"
+      :on-change="changeFile"
+      :before-upload="beforeUpload"
+      :http-request="upload"
+      :file-list="fileList"
+      :class="{ disabled: fileComputed }"
+    >
+      <i class="el-icon-plus" />
+    </el-upload>
+    <el-progress v-if="showPercent" style="width: 180px" :percentage="percent" />
+    <el-dialog title="图片" :visible.sync="showDialog">
+      <img :src="imgUrl" style="width:100%" alt="">
+    </el-dialog>
+  </div>
+</template>
+<script>
+import COS from 'cos-js-sdk-v5'
+const cos = new COS({
+  SecretId: 'AKIDHCtKZZO7VeX6PYOLyLDqE9sMkskdcdNq ', // 身份识别ID
+  SecretKey: 'OO9fy1jCO3SWVZO9etzxU6mBoDiRuvgD'// 身份秘钥
+
+})
+export default {
+  data() {
+    return {
+      fileList: [{
+        url: 'http://file01.16sucai.com/d/file/2011/0801/20110801111724537.jpg'
+      }], // 图片地址设置为数组
+      showDialog: false, // 控制显示弹层
+      imgUrl: '',
+      currentFileUid: '',
+      percent: 0,
+      showPercent: false
+    }
+  },
+  computed: {
+    // 判断是否已经上传完了一张
+    fileComputed() {
+      return this.fileList.length === 1
+    }
+  },
+  methods: {
+    preview(file) {
+      this.imgUrl = file.url
+      this.showDialog = true
+    },
+    handleRemove(file) {
+      // file是点击删除的文件
+      this.fileList = this.fileList.filter(item => item.uid !== file.uid)
+    },
+    changeFile(file, fileList) {
+      // 会执行多次
+      this.fileList = fileList.map(item => item)
+      // 上传成功 -> 数据才能进来 -> 腾讯云 OS
+    },
+    beforeUpload(file) {
+      // 文件类型、文件大小
+      const types = ['image/jpeg', 'image/gif', 'image/bmp', 'image/png']
+      if (!types.includes(file.type)) {
+        this.$message.error('上传图片只能是 JPG、GIF、BMP、PNG 格式!')
+        return false
+      }
+      // 检查大小
+      const maxSize = 10 * 1024 * 1024
+      if (maxSize < file.size) {
+        this.$message.error('图片大小最大不能超过10M')
+        return false
+      }
+      // 记住当前的 uid
+      this.currentFileUid = file.uid
+      this.showPercent = true
+      return true
+    },
+    upload(params) {
+      if (params.file) {
+        cos.putObject({
+          Bucket: 'ifer-1253924894', // 存储桶
+          Region: 'ap-nanjing', // 地域
+          Key: params.file.name, // 文件名
+          Body: params.file, // 要上传的文件对象
+          StorageClass: 'STANDARD', // 上传的模式类型 直接默认 标准模式即可
+          onProgress: (params) => {
+            this.percent = params.percent * 100
+          }
+        }, (err, data) => {
+          if (!err && data.statusCode === 200) {
+            this.fileList = this.fileList.map(item => {
+              if (item.uid === this.currentFileUid) {
+                return { url: 'http://' + data.Location, upload: true }
+              }
+              return item
+            })
+            setTimeout(() => {
+              this.showPercent = false // 隐藏进度条
+              this.percent = 0 // 进度归0
+            }, 2000)
+          }
+        })
+      }
+    }
+  }
+}
+</script>
+<style>
+.disabled .el-upload--picture-card {
+  display: none;
+}
+</style>
